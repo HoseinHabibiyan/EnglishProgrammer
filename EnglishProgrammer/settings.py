6594adcd59a9e6035e3459b96e6dbf033713eea8
Hosein Habibiyan <hn.habibiyan@gmail.com>
2023-10-26 01:20:34 +0330

    minio storage connection fix

--- EnglishProgrammer/settings.py
+++ EnglishProgrammer/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'tinymce',
     'storages',
+    'minio_storage'
 ]
 
 MIDDLEWARE = [
@@ -141,22 +142,22 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100 MB in bytes
 AWS_QUERYSTRING_AUTH = False
 
 DEFAULT_FILE_STORAGE = 'minio_storage.storage.MinioMediaStorage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STORAGES = {"default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
 
-AWS_S3_ENDPOINT_URL = 'https://refhub.storage.iran.liara.space'
+AWS_S3_ENDPOINT_URL = 'https://storage.iran.liara.space'
 AWS_S3_ACCESS_KEY_ID = 'm2prko9o0vhacb7n'
 AWS_S3_SECRET_ACCESS_KEY = 'a556090f-756f-4d42-88f6-531c580a0108'
 AWS_STORAGE_BUCKET_NAME = 'refhub'
 
-MINIO_STORAGE_ENDPOINT = 'refhub.storage.iran.liara.space'
+MINIO_STORAGE_ENDPOINT = 'storage.iran.liara.space'
 MINIO_STORAGE_ACCESS_KEY = 'm2prko9o0vhacb7n'
 MINIO_STORAGE_SECRET_KEY = 'a556090f-756f-4d42-88f6-531c580a0108'
 MINIO_SECRET_KEY = 'a556090f-756f-4d42-88f6-531c580a0108'
-MINIO_STORAGE_MEDIA_BUCKET_NAME = 'media'
+MINIO_STORAGE_MEDIA_BUCKET_NAME = 'refhub'
 
 
-MINIO_STORAGE_USE_HTTPS = False
+MINIO_STORAGE_USE_HTTPS = True
 MINIO_STORAGE_MEDIA_OBJECT_METADATA = {"Cache-Control": "max-age=1000"}
 MINIO_CONSISTENCY_CHECK_ON_START = True
 MINIO_QUERYSTRING_AUTH = False
@@ -170,5 +171,9 @@ MINIO_PUBLIC_BUCKETS = [
 MINIO_URL_EXPIRY_HOURS = timedelta(days=1)  # Default is 7 days (longest) if not defined
 
 
-STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/static/'
-MEDIA_URL =  f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/media/'
+# STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/static/'
+# MEDIA_URL =  f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/media/'
+
+STATIC_URL = '/static_files/'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR 
